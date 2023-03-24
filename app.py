6e57564e9746678795c8b1c3b7063bd2fc2b017b
Melrose Ceballos <melroseceballos@Melroses-MacBook-Pro.local>
2023-03-24 00:23:55 -0700

    created tie sequence

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def tictac_board (board):
     print("   ------------- ")
     print(f"   | {board[0][0]} | {board [0][1]} | {board [0][2]} |")
     print("   ------------- ")
-    print(f"   | {board [1][0]} | {board[1][1]} | {board[2][2]} | ")
+    print(f"   | {board [1][0]} | {board[1][1]} | {board[1][2]} | ")
     print("   ------------- ")
     print(f"   | {board[2][0]} | {board[2][1]} | {board[2][2]} | ")
     print("   ------------- ")
@@ -32,22 +32,50 @@ while True:
     os.system('clear')
     tictac_board(board)
 
+     # PLAYER X WIN LOGIC
+    if(board[0][0] == board [0][1] == board[0][2] == 'X' or
+board[0][0] == board [1][0] == board [2][0] == 'X' or
+board[1][0] == board[1][1] == board[2][2]== 'X' or
+board[2][0]==board[2][1]==board[2][2]=='X' or
+board[0][1]==board[1][1]==board[2][1]=='X' or
+board[0][2]==board[1][2]==board[2][2]=='X' or
+board[0][0]==board[1][1]==board[2][2]=='X' or
+board[0][2]==board[1][1]==board[2][0]=='X'):
+        print('Player X wins!')
+        break
+
+#TIE LOGIC
+# Check if the board is full
+    elif all([cell != str(i+1) for i in range(9) for row in board for cell in row]):
+        print("It's a tie!")
+        break
     #Player O
-    player_o = int(input("Player X: Place O in the board by selecting a number"))
+    player_o = int(input("Player O: Place O in the board by selecting a number"))
     row = (player_o - 1) // 3
     col = (player_o - 1) % 3
     board[row] [col] = 'O'
     os.system('clear')
     tictac_board(board)
 
-    # PROMPT IF PLAYERS CHOOSE SAME NUMBER
+    # PLAYER O WIN LOGIC
+    if(board[0][0] == board [0][1] == board[0][2] == 'O' or
+board[0][0] == board [1][0] == board [2][0] == 'O' or
+board[1][0] == board[1][1] == board[2][2]== 'O' or
+board[2][0]==board[2][1]==board[2][2]=='O' or
+board[0][1]==board[1][1]==board[2][1]=='O' or
+board[0][2]==board[1][2]==board[2][2]=='O' or
+board[0][0]==board[1][1]==board[2][2]=='O' or
+board[0][2]==board[1][1]==board[2][0]=='O'):
+        print('Player O wins!')
+        break
+         # PROMPT IF PLAYERS CHOOSE SAME NUMBER
     if player_x == player_o:
         print('Player O: Number already chosen by Player X, please choose a different number')
-        break
-    else:
+    elif player_o == player_x:
         print('Number already chosen by Player O, please choose a different number')
-        break
 
 
+
+    
 
 
