716e3403d66d16e046f0560f6dc92316f95f9d97
Melrose Ceballos <melroseceballos@Melroses-MacBook-Pro.local>
2023-03-23 23:50:18 -0700

    created error logic

--- app.py
+++ app.py
@@ -22,224 +22,32 @@ board = [['1','2','3'], ['4','5','6'],['7','8','9']]
 
 tictac_board(board)
 
+#GAME LOOP HERE
+    #Player X
+while True:
+    player_x = int(input("Place X in board by selecting number"))
+    row = (player_x - 1) // 3
+    col = (player_x - 1) % 3
+    board[row][col] = 'X'
+    os.system('clear')
+    tictac_board(board)
+
+    #Player O
+    player_o = int(input("Player X: Place O in the board by selecting a number"))
+    row = (player_o - 1) // 3
+    col = (player_o - 1) % 3
+    board[row] [col] = 'O'
+    os.system('clear')
+    tictac_board(board)
+
+    # PROMPT IF PLAYERS CHOOSE SAME NUMBER
+    if player_x == player_o:
+        print('Player O: Number already chosen by Player X, please choose a different number')
+        break
+    else:
+        print('Number already chosen by Player O, please choose a different number')
+        break
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# Define a 3x3 list to represent the Tic Tac Toe board.
-    #put numbers in variables and then use .replace to change it to the shape X|O?
-# print("     B O A R D ")
-# print("   ------------- ")
-# top_three = ('1', '2', '3')
-# print(f"   | {top_three[0]} | {top_three[1]} | {top_three[2]} |")
-# print("   ------------- ")
-# middle = ('4', '5', '6')
-# print(f"   | {middle[0]} | {middle[1]} | {middle[2]} | ")
-# print("   ------------- ")
-# bottom = ('7', '8', '9')
-# print(f"   | {bottom[0]} | {bottom[1]} | {bottom[2]} | ")
-# print("   ------------- ")
-# print('\n')
-
-# user_x = int(input ("Player X, Select a number you would like X to go?"))
-# print(user_x)
-
-# # To change 1 (player X)
-# if (user_x == 1):
-#    top_three = f" | 'X' | {top_three[1]} | {top_three[2]} | .replace('1','X')"
-# print("     B O A R D ")
-# print("   ------------- ")
-# top_three = ('1', '2', '3')
-# print(f"   | X | {top_three[1]} | {top_three[2]} |")
-# print("   ------------- ")
-# middle = ('4', '5', '6')
-# print(f"   | {middle[0]} | {middle[1]} | {middle[2]} | ")
-# print("   ------------- ")
-# bottom = ('7', '8', '9')
-# print(f"   | {bottom[0]} | {bottom[1]} | {bottom[2]} | ")
-# print("   ------------- ")
-# print('\n')
-# # to change 2
-# if (user_x == 1):
-#    top_three = f" | {top_three[0]} | 'X' | {top_three[2]} | .replace('2','X')"
-# print("     B O A R D ")
-# print("   ------------- ")
-# top_three = ('1', '2', '3')
-# print(f"   | {top_three[0]} | X | {top_three[2]} |")
-# print("   ------------- ")
-# middle = ('4', '5', '6')
-# print(f"   | {middle[0]} | {middle[1]} | {middle[2]} | ")
-# print("   ------------- ")
-# bottom = ('7', '8', '9')
-# print(f"   | {bottom[0]} | {bottom[1]} | {bottom[2]} | ")
-# print("   ------------- ")
-# print('\n')
-
-# # User X 3
-# if (user_x == 3):
-#    top_three = f" | {top_three[0]} | {top_three[1]} | 'X' | .replace('3','X')"
-# print("     B O A R D ")
-# print("   ------------- ")
-# top_three = ('1', '2', '3')
-# print(f"   | {top_three[0]} | {top_three[1]} | X |")
-# print("   ------------- ")
-# middle = ('4', '5', '6')
-# print(f"   | {middle[0]} | {middle[1]} | {middle[2]} | ")
-# print("   ------------- ")
-# bottom = ('7', '8', '9')
-# print(f"   | {bottom[0]} | {bottom[1]} | {bottom[2]} | ")
-# print("   ------------- ")
-# print('\n')
-
-# # user-x 4
-# if (user_x == 4):
-#    middle = f" | 'X' | {middle[1]}  | {middle[2]} | .replace('4','X')"
-# print("     B O A R D ")
-# print("   ------------- ")
-# top_three = ('1', '2', '3')
-# print(f"   | {top_three[0]} | {top_three[1]} | {top_three[2]} |")
-# print("   ------------- ")
-# middle = ('4', '5', '6')
-# print(f"   | X | {middle[1]} | {middle[2]} | ")
-# print("   ------------- ")
-# bottom = ('7', '8', '9')
-# print(f"   | {bottom[0]} | {bottom[1]} | {bottom[2]} | ")
-# print("   ------------- ")
-# print('\n')
-
-# #USER X 5
-# if (user_x == 5):
-#    middle = f" | {middle[0]} | 'X'  | {middle[2]} | .replace('5','X')"
-# print("     B O A R D ")
-# print("   ------------- ")
-# top_three = ('1', '2', '3')
-# print(f"   | {top_three[0]} | {top_three[1]} | {top_three[2]} |")
-# print("   ------------- ")
-# middle = ('4', '5', '6')
-# print(f"   | {middle[0]} | X | {middle[2]} | ")
-# print("   ------------- ")
-# bottom = ('7', '8', '9')
-# print(f"   | {bottom[0]} | {bottom[1]} | {bottom[2]} | ")
-# print("   ------------- ")
-# print('\n')
-
-# #USER X-6
-# if (user_x == 6):
-#    middle = f" | {middle[0]} | {middle[1]}  | 'X' | .replace('6','X')"
-# print("     B O A R D ")
-# print("   ------------- ")
-# top_three = ('1', '2', '3')
-# print(f"   | {top_three[0]} | {top_three[1]} | {top_three[2]} |")
-# print("   ------------- ")
-# middle = ('4', '5', '6')
-# print(f"   | {middle[0]} | {middle[1]} | X | ")
-# print("   ------------- ")
-# bottom = ('7', '8', '9')
-# print(f"   | {bottom[0]} | {bottom[1]} | {bottom[2]} | ")
-# print("   ------------- ")
-# print('\n')
-
-# #USER X-7
-# if (user_x == 4):
-#    bottom = f" | 'X' | {bottom[1]} | {bottom[2]} | .replace('7','X')"
-# print("     B O A R D ")
-# print("   ------------- ")
-# top_three = ('1', '2', '3')
-# print(f"   | {top_three[0]} | {top_three[1]} | {top_three[2]} |")
-# print("   ------------- ")
-# middle = ('4', '5', '6')
-# print(f"   | {middle[0]} | {middle[1]} | {middle[2]} | ")
-# print("   ------------- ")
-# bottom = ('7', '8', '9')
-# print(f"   | X | {bottom[1]} | {bottom[2]} | ")
-# print("   ------------- ")
-# print('\n')
-
-# #USER X-8
-# if (user_x == 4):
-#    bottom = f" | {bottom[0]} | 'X'| {bottom[2]} | .replace('8','X')"
-# print("     B O A R D ")
-# print("   ------------- ")
-# top_three = ('1', '2', '3')
-# print(f"   | {top_three[0]} | {top_three[1]} | {top_three[2]} |")
-# print("   ------------- ")
-# middle = ('4', '5', '6')
-# print(f"   | {middle[0]} | {middle[1]} | {middle[2]} | ")
-# print("   ------------- ")
-# bottom = ('7', '8', '9')
-# print(f"   | {bottom[0]} | X | {bottom[2]} | ")
-# print("   ------------- ")
-# print('\n')
-
-# #USER X-9
-# if (user_x == 4):
-#    bottom = f" | {bottom[0]}| {bottom[1]} | 'X' | .replace('9','X')"
-# print("     B O A R D ")
-# print("   ------------- ")
-# top_three = ('1', '2', '3')
-# print(f"   | {top_three[0]} | {top_three[1]} | {top_three[2]} |")
-# print("   ------------- ")
-# middle = ('4', '5', '6')
-# print(f"   | {middle[0]} | {middle[1]} | {middle[2]} | ")
-# print("   ------------- ")
-# bottom = ('7', '8', '9')
-# print(f"   | {bottom[0]} | {bottom[1]} | X | ")
-# print("   ------------- ")
-# print('\n')
-
-
-# # PLAYER O-1
-# user_O = int(input('Player O, select a number you would like O to go')) 
-# if (user_O == 1):
-#    top_three = f" | 'O' | {top_three[1]} | {top_three[2]} | .replace('1','O')"
-# print("     B O A R D ")
-# print("   ------------- ")
-# top_three = ('1', '2', '3')
-# print(f"   | O | {top_three[1]} | {top_three[2]} |")
-# print("   ------------- ")
-# middle = ('4', '5', '6')
-# print(f"   | {middle[0]} | {middle[1]} | {middle[2]} | ")
-# print("   ------------- ")
-# bottom = ('7', '8', '9')
-# print(f"   | {bottom[0]} | {bottom[1]} | X | ")
-# print("   ------------- ")
-# print('\n')
-
-# #PLAYER 0-2
-# if (user_O == 2):
-#    top_three = f" | {top_three[0]} | 'O' | {top_three[2]} | .replace('2','O')"
-# print("     B O A R D ")
-# print("   ------------- ")
-# top_three = ('1', '2', '3')
-# print(f"   | {top_three[0]} | O | {top_three[2]} |")
-# print("   ------------- ")
-# middle = ('4', '5', '6')
-# print(f"   | {middle[0]} | {middle[1]} | {middle[2]} | ")
-# print("   ------------- ")
-# bottom = ('7', '8', '9')
-# print(f"   | {bottom[0]} | {bottom[1]} | X | ")
-# print("   ------------- ")
-# print('\n')
-
-# # Define a function to check if a player has won.
-# # Define a function to alternate between players and execute their moves.
-# # Define a function to check if the game is over.
-# # Define a main function that executes the game logic
